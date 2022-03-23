027345e0fba2b0c58d91a9dfaf06d020afeb4ec2
lan666as <4zidan@gmail.com>
2022-03-23 10:32:36 +0700

    add initial Koreksi function

---
+++ azure-functions/GetKoreksi/__init__.py
@@ -0,0 +1,29 @@
+import logging
+
+import azure.functions as func
+import json
+
+
+def main(req: func.HttpRequest, docs:func.DocumentList) -> func.HttpResponse:
+    logging.info('Python GetKoreksi function processed a request.')
+
+    koreksi_list = []
+
+    for doc in docs:
+        koreksi = {
+            "document" : doc["document"],
+            "sentences" : []
+        }
+
+        for sentence in doc["sentences"]:
+            koreksi["sentences"].append({
+                "sentence" : sentence["sentence"]
+            })
+
+        koreksi_list.append(koreksi)
+
+    return func.HttpResponse(
+        json.dumps(koreksi_list),
+        status_code=200,
+        mimetype="application/json"            
+    )
