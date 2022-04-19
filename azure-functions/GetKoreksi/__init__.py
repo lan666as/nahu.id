1614e62e724d1070a8d4c8628f87f897c0a836f4
lan666as <4zidan@gmail.com>
2022-04-19 21:37:38 +0700

    add proposed azure architecture

--- azure-functions/GetKoreksi/__init__.py
+++ azure-functions/GetKoreksi/__init__.py
@@ -4,27 +4,21 @@ import azure.functions as func
 import json
 
 
-def main(req: func.HttpRequest, docs:func.DocumentList) -> func.HttpResponse:
-    logging.info('Python GetKoreksi function processed a request.')
+def main(req: func.HttpRequest, docs: func.DocumentList) -> func.HttpResponse:
+    logging.info("Python GetKoreksi function processed a request.")
 
     koreksi_list = []
 
     for doc in docs:
-        koreksi = {
-            "document" : doc["document"],
-            "sentences" : []
-        }
+        koreksi = {"document": doc["document"], "sentences": []}
 
         for sentence in doc["sentences"]:
-            koreksi["sentences"].append({
-                "sentence" : sentence["sentence"],
-                "koreksi" : sentence["koreksi"]
-            })
+            koreksi["sentences"].append(
+                {"sentence": sentence["sentence"], "koreksi": sentence["koreksi"]}
+            )
 
         koreksi_list.append(koreksi)
 
     return func.HttpResponse(
-        json.dumps(koreksi_list),
-        status_code=200,
-        mimetype="application/json"            
+        json.dumps(koreksi_list), status_code=200, mimetype="application/json"
     )
